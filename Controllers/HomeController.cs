8dc004e91c0c143a495ea236d15c4fc82be46ec8
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-04-21 23:26:08 +0300

    Viewbag ve Tempdata ile aynı controller içinde veri alışverişi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
             //ViewData["check1"] = true;//Aynı keyden birden fazla verilirse diğerleri ezilir en son verilen geçerlidir.
 
             //ViewData gibi işlev gören fakat dinamic yapıda olan çalışma zamanında kontrolleri ekleyen,kullanıcı dostu yazımı olan viewbag ile de aynı
-            //işlem yapılabilir.
+            //işlem yapılabilir.ViewBag'de aslında ViewDatayı kullandığını unutmayalım
             //ViewBag.AdSoyad = "Mehmet Elbeyli";
             //ViewBag.check1 = true;
 
@@ -24,6 +24,27 @@ namespace WebApplication1.Controllers
             TempData["AdSoyad"] = "Mehmet Elbeyli";
             TempData["check1"] = true;
 
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Ad = "Mehmet";
+            ViewBag.Durum = true;
+            ViewBag.List1 = new SelectListItem[]
+            {
+                new SelectListItem(){Text="Mehmet"},
+                new SelectListItem() { Text = "Ahmet" },
+                new SelectListItem(){Text="Hasan"}
+            };
+            TempData["t1"] = ViewBag.Ad;//Contact'da burdaki viewbag'i kullanması için tempdataya attık
+            TempData["d1"] = ViewBag.Durum;
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Ad = TempData["t1"];//Aynı şekilde viewbag aynı datayı kullanması için temdatadan aldık
+            ViewBag.Durum = TempData["d1"];
 
             return View();
         }
